df7d70b4bd02db598ec1a7a94533867ec124d1d5
Redtides0 <2607964128@qq.com>
2024-07-24 00:33:57 +0800

    fix llama3 prompt

--- social_simulation/social_platform/config/user.py
+++ social_simulation/social_platform/config/user.py
@@ -26,13 +26,13 @@ class UserInfo:
 
         system_content = f"""
 # OBJECTIVE
-You're a Twitter user, and I'll present you with some tweets. After you see the tweets, choose some actions from the following functions.
+You're a Twitter user, and I'll present you with some posts. After you see the posts, choose some actions from the following functions.
 
-- do_nothing: Most of the time, you just don't feel like retweeting or liking a tweet, and you just want to look at it. In such cases, choose this action "do_nothing"
-- retweet: Retweet a tweet.
-    - Arguments: "tweet_id" (integer) - The ID of the tweet to be retweeted. You can `retweet` when you want to spread it.
-- like: Likes a specified tweet.
-    - Arguments: "tweet_id" (integer) - The ID of the tweet to be liked. You can `like` when you feel something interesting or you agree with.
+- do_nothing: Most of the time, you just don't feel like reposting or liking a post, and you just want to look at it. In such cases, choose this action "do_nothing"
+- repost: Repost a post.
+    - Arguments: "post_id" (integer) - The ID of the post to be reposted. You can `repost` when you want to spread it.
+- like: Likes a specified post.
+    - Arguments: "post_id" (integer) - The ID of the post to be liked. You can `like` when you feel something interesting or you agree with.
 - follow: Follow a user specified by 'followee_id'. You can `follow' when you respect someone, love someone, or care about someone.
     - Arguments: "followee_id" (integer) - The ID of the user to be followed.
 
@@ -46,7 +46,7 @@ Your actions should be consistent with your self-description and personality.
 Your answer should follow the response format:
 
 {{
-    "reason": "your feeling about these tweets and users, then choose some functions based on the feeling. Reasons and explanations can only appear here.",
+    "reason": "your feeling about these posts and users, then choose some functions based on the feeling. Reasons and explanations can only appear here.",
     "functions": [{{
         "name": "Function name 1",
         "arguments": {{
