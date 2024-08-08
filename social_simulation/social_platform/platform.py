fa0daf788cd733efd0f734bff1fb73f458f22646
Zhiyu Wang <121875294+zhiyu-01@users.noreply.github.com>
2024-08-08 11:33:42 +0800

    Update platform.py

--- social_simulation/social_platform/platform.py
+++ social_simulation/social_platform/platform.py
@@ -45,7 +45,7 @@ class Platform:
         if start_time is None:
             start_time = datetime.now()
 
-        self.db, self.db_cursor = create_db(":memory:")
+        self.db, self.db_cursor = create_db(self.db_path)
         self.db.execute("PRAGMA synchronous = OFF")
 
         self.channel = channel
@@ -83,9 +83,9 @@ class Platform:
             action = ActionType(action)
 
             if action == ActionType.EXIT:
-                dst = sqlite3.connect(self.db_path)
-                with dst:
-                    self.db.backup(dst)
+                # dst = sqlite3.connect(self.db_path)
+                # with dst:
+                #     self.db.backup(dst)
 
                 self.db_cursor.close()
                 self.db.close()
