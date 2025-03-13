3a10c1175a3024492c34a5b625e74e482c946566
liwei <liwei@madacode.com>
2025-03-13 18:07:59 +0800

    remove debug content

--- scripts/twitter_game/twitter_simulation.py
+++ scripts/twitter_game/twitter_simulation.py
@@ -155,8 +155,6 @@ async def running(
     await trigger(step, content)
 
     channel_name = f"predict_new_{content_id}"
-    print("===============================")
-    print(channel_name)
     pubsub = redis.pubsub()
     pubsub.subscribe(channel_name)
 
