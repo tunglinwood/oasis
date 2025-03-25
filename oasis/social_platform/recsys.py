06543da2889d75c6586b791c9ed25dd3132c8ee1
yiyiyi0817 <1359513271@qq.com>
2025-03-25 16:03:19 +0800

    change embedding model but error

--- oasis/social_platform/recsys.py
+++ oasis/social_platform/recsys.py
@@ -1,12 +1,12 @@
 # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
-# Licensed under the Apache License, Version 2.0 (the “License”);
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an “AS IS” BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -30,7 +30,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from transformers import AutoModel, AutoTokenizer
 
-from .process_recsys_posts import generate_post_vector
+from .process_recsys_posts import (
+    generate_post_vector,
+    generate_post_vector_openai
+)
 from .typing import ActionType, RecsysType
 
 rec_log = logging.getLogger(name='social.rec')
@@ -418,7 +421,8 @@ def rec_sys_personalized_twh(
         max_rec_post_len: int,
         # source_post_indexs: List[int],
         recall_only: bool = False,
-        enable_like_score: bool = False) -> List[List]:
+        enable_like_score: bool = False,
+        use_openai_embedding: bool = False) -> List[List]:
     # Set some global variables to reduce time consumption
     global date_score, fans_score, t_items, u_items, user_previous_post
     global user_previous_post_all, user_profiles
@@ -529,10 +533,14 @@ def rec_sys_personalized_twh(
         corpus = user_profiles + filtered_posts_tuple[0]
         # corpus = user_profiles + list(t_items.values())
         tweet_vector_start_t = time.time()
-        all_post_vector_list = generate_post_vector(twhin_model,
-                                                    twhin_tokenizer,
-                                                    corpus,
-                                                    batch_size=1000)
+        if use_openai_embedding:
+            all_post_vector_list = generate_post_vector_openai(
+                corpus, batch_size=1000)
+        else:
+            all_post_vector_list = generate_post_vector(twhin_model,
+                                                        twhin_tokenizer,
+                                                        corpus,
+                                                        batch_size=1000)
         tweet_vector_end_t = time.time()
         rec_log.info(
             f"twhin model cost time: {tweet_vector_end_t-tweet_vector_start_t}"
